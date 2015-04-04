7e52fdab8a091dba9113e84614a002535f5849bc
Anish Athalye <me@anishathalye.com>
2015-04-03 21:34:18 -0400

    Complete user schema

--- models/user.js
+++ models/user.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
   username: {type: String, unique: true, required: true},
-  password: {type: String, required: true},
+  active: Boolean,
+  latitude: Number,
+  longitude: Number,
+  classes: [{name: String, status: {type: String, enum: ['TODO', 'STARTED', 'DONE']}}]
 });
 
 var User = mongoose.model('User', userSchema);
