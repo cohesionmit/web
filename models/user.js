3cb89e11379b44e348df3c6f57b8eb71da058ffe
Anish Athalye <me@anishathalye.com>
2015-04-05 22:57:09 -0400

    Remove active

--- models/user.js
+++ models/user.js
@@ -4,7 +4,6 @@ var userSchema = new mongoose.Schema({
   firstname: {type: String, required: true},
   lastname: {type: String, required: true},
   fburl: {type: String, unique: true, required: true},
-  active: {type: Boolean, default: false, required: true},
   location: {type: [Number], default: [0.0, 0.0], index: '2d'},
   lastupdate: {type: Date, default: new Date(0), required: true},
   classes: [{name: String, status: {type: String, enum: ['TODO', 'STARTED', 'DONE']}}]
